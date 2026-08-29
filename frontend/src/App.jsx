01fd12869b712f79583f1c034ee0bbcae1f48d49
Christie Jude Tarre <cltarre@up.edu.ph>
2026-08-30 02:28:53 +0800

    feat: initialize project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {Toaster} from 'sonner';
+import AppRoutes from './routes/AppRoutes.jsx';
+
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+function AppRoutes(){
+    return(
+        <div>
+            <h1>Project Name</h1>
+            <p>Application is running.</p>
+        </div>
+    );
+}
+
+export default AppRoutes;
